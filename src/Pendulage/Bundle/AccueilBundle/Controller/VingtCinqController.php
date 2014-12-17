7185a120ca6f374489e8ad78a265dbc2a3d5a3eb
mpassik <mpassik@MPASSIK-CRAIL.groupe.sa.colas.com>
2014-12-17 14:55:03 +0100

    calc+tab

--- src/Pendulage/Bundle/AccueilBundle/Controller/VingtCinqController.php
+++ src/Pendulage/Bundle/AccueilBundle/Controller/VingtCinqController.php
@@ -5,58 +5,56 @@ namespace Pendulage\Bundle\AccueilBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
-class VingtCinqController extends Controller
-{
-    public function ListerAction()
-    {
+class VingtCinqController extends Controller {
+
+    public function ListerAction() {
+
         $repo = $this->getDoctrine()->getRepository('PendulageAccueilBundle:Calcul');
-        $listeVingtCinq = $repo->findAll();
+        $calculData = $repo->findBy(array('spanlenth' => 49.5));
+        $varPortee = 51;
+        $temp=0;
         
-        //fonction obtenir le nombre de pendule
-//        $NbPendules = $repo->findBy(array('portee'=>0));
-        $NbPendules=40;
-        if ($NbPendules<=54.5 && $NbPendules>50) {
+        if($varPortee>50){
             $temp=8;
         }
-        elseif ($NbPendules<=50 && $NbPendules>45.5) {
+        elseif ($varPortee<=50 && $varPortee>45.5) {
             $temp=7;
         }
-        elseif ($NbPendules<=45.5 && $NbPendules>41) {
+        elseif ($varPortee<=45.5 && $varPortee>41) {
             $temp=7;
         }
-        elseif ($NbPendules<=41 && $NbPendules>36.5) {
+        elseif ($varPortee<=41 && $varPortee>36.5) {
             $temp=6;
         }
-        elseif ($NbPendules<=36.5 && $NbPendules>32) {
+        elseif ($varPortee<=36.5 && $varPortee>32) {
             $temp=6;
         }
-        elseif ($NbPendules<=32&& $NbPendules>0) {
+        elseif ($varPortee<=32 && $varPortee>0) {
             $temp=5;
-        }else{
+        }
+        elseif ($varPortee<=0) {
             $temp=0;
         }
-        
-        
+
         return $this->render('PendulageAccueilBundle:VingtCinq:Lister.html.twig', array(
-              "listeVingtCinq"=>$listeVingtCinq,
-                "temp"=>$temp
-            ));    }
+            "temp"=>$temp
+        ));
+    }
 
-    public function RemoveAction()
-    {
+    public function RemoveAction() {
         return $this->render('PendulageAccueilBundle:VingtCinq:Remove.html.twig', array(
-                // ...
-            ));    }
+                        // ...
+        ));
+    }
 
-    public function AddAction(Request $request)
-    {
+    public function AddAction(Request $request) {
         $ohle = new \Pendulage\Bundle\AccueilBundle\Entity\Calcul;
         $form = $this->createForm(new \Pendulage\Bundle\AccueilBundle\Form\AddOhleForm(), $ohle);
-        
+
         if ($this->getRequest()->isMethod('POST')) {
-        
-        $form->handleRequest($request);
-            if($form->isValid()){
+
+            $form->handleRequest($request);
+            if ($form->isValid()) {
                 $em = $this->getDoctrine()->getManager();
                 $em->persist($ohle);
                 $em->flush();
@@ -66,63 +64,28 @@ class VingtCinqController extends Controller
                 return $this->redirect($this->generateUrl("vingtCinq_lister"));
             }
         }
-        
+
         return $this->render('PendulageAccueilBundle:VingtCinq:Add.html.twig', array(
-                "form" => $form->createView()
-            ));    }
+                    "form" => $form->createView()
+        ));
+    }
 
-    public function ModifierAction()
-    {
+    public function ModifierAction() {
         return $this->render('PendulageAccueilBundle:VingtCinq:Modifier.html.twig', array(
-                // ...
-            ));    }
+                        // ...
+        ));
+    }
 
-    public function FindAction()
-    {
+    public function FindAction() {
         return $this->render('PendulageAccueilBundle:VingtCinq:Find.html.twig', array(
-                // ...
-            ));    }
+                        // ...
+        ));
+    }
 
-    public function FindByAction()
-    {
+    public function FindByAction() {
         return $this->render('PendulageAccueilBundle:VingtCinq:FindBy.html.twig', array(
-                // ...
-            ));    }
+                        // ...
+        ));
+    }
     
-    public function NbPendulesAction()
-    {
-        $repo = $this->getDoctrine()->getRepository('PendulageAccueilBundle:Calcul');
-        $listeVingtCinq = $repo->findAll();
-        
-        //function NbPendules
-        function NbPendules($varPortee){
-            if($varPortee<= 0){
-                $temp=0;
-            }
-            else if($varPortee<= 32){
-                $temp=5;
-            }
-            else if($varPortee<= 36.5){
-                $temp=6;
-            }
-            else if($varPortee<= 41){
-                $temp=6;
-            }
-            else if($varPortee<= 45.5){
-                $temp=7;
-            }
-            else if($varPortee<= 50){
-                $temp=7;
-            }
-            else if($varPortee<= 54.5){
-                $temp=8;
-            }
-            return $temp;
-        }
-        
-        return $this->render('PendulageAccueilBundle:VingtCinq:Lister.html.twig', array(
-              "listeVingtCinq"=>$listeVingtCinq ,
-                "temp"=>  $temp
-            ));    }
-
 }
